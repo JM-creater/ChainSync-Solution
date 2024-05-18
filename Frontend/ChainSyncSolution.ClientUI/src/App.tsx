625aa03d3ba53ba4ca149a3df0eaec0fc58b0365
Joseph Martin Garado <garadojosephmartin98@gmail.com>
2024-05-19 07:22:45 +0800

    Added Files in Frontend

--- Frontend/ChainSyncSolution.ClientUI/src/App.tsx
+++ Frontend/ChainSyncSolution.ClientUI/src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import LoginScreen from './pages/Authfeatures/LoginScreen'
 
 function App() {
 
   return (
     <React.Fragment>
-      <div>
-        test
-      </div> 
+      <BrowserRouter>
+        <Routes>
+          <Route index element={ <LoginScreen /> } />
+        </Routes>
+      </BrowserRouter>
     </React.Fragment>
   )
 }
